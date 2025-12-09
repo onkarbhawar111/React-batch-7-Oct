b543e65e3cb9f03fe20f115b812424e95c9ad413
onkarbhawar111 <onkarbhawar111@gmail.com>
2025-12-09 16:16:41 +0530

    9/12

--- demo1/src/components/Form Validation/signUpValidations.jsx
+++ demo1/src/components/Form Validation/signUpValidations.jsx
@@ -5,5 +5,5 @@ export const signUpValidation = Yup.object({
     name: Yup.string().required('Name is required....!!!!'), 
     email: Yup.string().email().required('Email is compulsory...'),
     password: Yup.string().min(3).max(10).required('Password is required...'),
-    cpassword: Yup.string().oneOf([Yup.ref('password')], "Password not matched..").required('Compulsory field !!!')
+    cpassword: Yup.string().oneOf([Yup.ref('password')], "Password not matched...").required('Compulsory field !!!')
 })
